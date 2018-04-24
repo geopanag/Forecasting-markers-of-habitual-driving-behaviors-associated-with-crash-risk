95a4e2e9ad1eaf6f3b01163958093876aa31e61c
Gpan <georgepanagopoulos5@yahoo.gr>
2018-04-24 11:05:44 -0500

    run corrections

--- _6_Run.R
+++ _6_Run.R
@@ -4,14 +4,12 @@
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-run_seq = c("_1_creat_simulation_dataset.R",
+run_seq = c("_1_create_simulation_dataset.R",
             "_2_feature_extraction.R",
             "_3_feature_selection.R",
             "_4_classification.R",
             "_5_plot_results.R")
 
 
-source("_1_create_simulation_dataset.R")
-source("_2_feature_extraction.R")
 sapply(run_seq,source)
 
